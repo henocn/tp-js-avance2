6f6293f88d5611fb07244e10478c37cfdfe3096f
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-10 13:12:37 +0000

    labels on form

--- components/Form.jsx
+++ components/Form.jsx
@@ -3,19 +3,22 @@ import React from 'react'
 const Form = () => {
   return (
     <div>
-      <form action="POST">
+      <form method="POST" className='form'>
         <div className="name-div">
-          <label htmlFor="name" className="name-label"></label>
+          <label htmlFor="name" className="name-label">Your name</label>
           <input type="text" className="name-input" id='name' name='name' />
         </div>
         <div className="email-div">
-          <label htmlFor="" className="email-label"></label>
+          <label htmlFor="" className="email-label">Enter email</label>
           <input type="email" className="email-input" id='email' name='email' />
 
         </div>
         <div className="phone-div">
-          <label htmlFor="phone" className="phone-label"></label>
+          <label htmlFor="phone" className="phone-label">Phone number</label>
           <input type="text" className="phone-input" id='phone' name='phone' />
+        </div>
+        <div className="submit-div">
+          <input type="submit" className="submit" />
         </div>
       </form>
     </div>
