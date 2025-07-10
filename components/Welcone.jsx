f43360184f903e3e7d3b76ae2e3d557b5b7cee29
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-10 12:15:56 +0000

    Add of article's component

--- components/Welcone.jsx
+++ components/Welcone.jsx
@@ -5,12 +5,14 @@ const Welcone = () => {
   return (
     <div>
         <h2>Liste des collaborateurs</h2>
-        {colaborators.map((col, _) => (
-            <div key={_}>
-                <h4>{col.name}</h4>
-                <h5> {col.role} </h5>
-            </div>
-        ))}
+        <div className="colab-container">
+          {colaborators.map((col, _) => (
+              <div key={_} className='colab'>
+                  <h4>{col.name}</h4>
+                  <h5> {col.role} </h5>
+              </div>
+          ))}
+        </div>
 
     </div>
   )
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {articles} from '../data/articles'
 import Compteur from '../components/Compteur'
+import Article from '../components/Article'
 
 const Home = () => {
-  return (<>
+  return (
+  <>
     <h1>Bienvenu</h1>
     <Compteur />
-
+    <Article articles={articles} />
   </>)
 }
 
