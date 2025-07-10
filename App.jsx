7f37b4e4a2d47cceee4bff23cfd523871dcfb9ba
Henoc N'GASAMA <ngasamah@gmail.com>
2025-07-10 13:02:12 +0000

    Specifying routes

--- App.jsx
+++ App.jsx
@@ -2,6 +2,8 @@
 import "./App.css"   
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Form from "./pages/Form"
+import Contact from "./pages/Contact"
 import { Routes, Route } from "react-router-dom"
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/form" element={<Form/>} />
         </Routes>
         
     )
